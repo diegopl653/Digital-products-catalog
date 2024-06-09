82f5ecb16620c036309c58643c88649426f592ee
Diego Perez <Diego.fernando.perez.luna@gmail.com>
2024-06-09 13:52:41 -0400

    feat: header and first section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#FFC4D6",
+				secondary: "#C4FFCB",
+				darkness: "#804456",
+				hover: "#78AD7E"
+			},
+			fontFamily: {
+				'brand': [ "Pacifico" , 'sans-serif'],
+			}
+		},
 	},
 	plugins: [],
 }
